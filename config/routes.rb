1d2b6f315a640b494d9988d8e922365775cc1775
Hadrien Bourgoin <hadrienbourgoin@gmail.com>
2023-10-06 17:30:01 +0200

    main page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'projects#index'
+  resources :projects, only: %i[index show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Project.create(name: 'DocG', description: 'DocG is a powerful new tool that allow you to generate configuration
+  documents such as JSON or XML files. With its interface that is both user friendly and professional oriented,
+  you will easily be able to create higly customizable documents from files on your computers.')
+
+Project.create(name: 'NoteMe', description: 'NoteMe is an application designed and though for teachers. With
+  its ergonomic design it is super easy to use for you best comfort! Create your classes, take notes on
+  your students and improve your teaching experience!')
